aebbe559e5514b01d3e310b8d786f871de01ab8c
Andreas M. Antonopoulos <andreas@antonopoulos.com>
2015-09-22 13:53:07 -0500

    version increment for workingsigs branch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='multisig-recovery',
-    version = '0.1',
+    version = '0.2-workingsigs',
     packages=[
         'multisigrecovery',
         'tests'
